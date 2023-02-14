de469deb6bc1bacc969aaa8042cef37844f35452
Roman <55388694+Roman-wdesign@users.noreply.github.com>
2023-02-14 17:31:50 +0700

    prepare Cart II

--- src/Cards/card-list/CardsList.vue
+++ src/Cards/card-list/CardsList.vue
@@ -46,14 +46,8 @@ const itemClick = (name: string) => {
       <h2 class="flex justify-center text-4xl font-extrabold mb-8">Card List</h2>
     </div>
     <div class="flex flex-row justify-center static flex-wrap mb-8">
-      <CardItem 
-      class="my-3 mx-7 hover:shadow-2xl"
-       v-for="card in cards" 
-       :key="card.id" 
-       :card__data="card"
-        @itemClick="itemClick"
-        @addToCart="addToCart" 
-        >List Rendering
+      <CardItem class="my-3 mx-7 hover:shadow-2xl" v-for="card in cards" :key="card.id" :card__data="card"
+        @itemClick="itemClick" @addToCart="addToCart">List Rendering
       </CardItem>
     </div>
   </div>
