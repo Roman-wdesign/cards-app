4b48cf2d9b9b4c51a1d4506238a6340dd4567b60
Roman-wdesign <klauss.elephant@gmail.com>
2023-02-15 10:15:42 +0700

    linter and prettier rules

--- src/Cards/card-item/CardItem.vue
+++ src/Cards/card-item/CardItem.vue
@@ -1,58 +1,62 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import ButtonMain from "@/shared/components/buttons/ButtonMain.vue";
+import ButtonMain from '@/shared/components/buttons/ButtonMain.vue'
 
-
-// props from CardList.vue 
+// props from CardList.vue
 
 export interface cardData {
-  name: string, image: string
-  id:number
+  name: string
+  image: string
+  id: number
 }
 
 export interface Props {
   card__data: cardData
 }
-const props = defineProps<Props>();
+const props = defineProps<Props>()
 
 const emit = defineEmits<{
   (e: 'itemClick', name: string): string
-  (e: 'addToCart',  cardData:string): string
+  (e: 'addToCart', cardData: string): string
 }>()
 
 // event to CardList.vue with argument data.name
 const buttonClick = ref((name: string): string => emit('itemClick', name))
-const addToCartClick = ref((cardData: string): string => emit('addToCart', cardData))
-
+const addToCartClick = ref((cardData: string): string =>
+  emit('addToCart', cardData)
+)
 </script>
 
 <template>
   <main>
     <!--    pass up event to CardList-->
     <div class="Card">
-
       <div
-        class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {{ props.card__data.name }} </h5>
+        class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+      >
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {{ props.card__data.name }}
+        </h5>
         <img class="rounded-t-lg" :src="props.card__data.image" alt="" />
 
         <div class="flex flex-row justify-between">
           <ButtonMain
             class="text-white bg-blue-600 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mt-5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            @click="buttonClick(props.card__data.name)">
+            @click="buttonClick(props.card__data.name)"
+          >
             More
           </ButtonMain>
           <ButtonMain
             class="text-blue-600 bg-white border-2 border-blue-600 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mt-5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            @click="addToCartClick(props.card__data.name)">
+            @click="addToCartClick(props.card__data.name)"
+          >
             Add to Cart
           </ButtonMain>
         </div>
       </div>
       <hr />
     </div>
-
   </main>
 </template>
--- src/Cards/card-list/CardsList.vue
+++ src/Cards/card-list/CardsList.vue
@@ -1,31 +1,27 @@
 <script setup lang="ts">
-import CardItem from "@/Cards/card-item/CardItem.vue";
-import { computed, onMounted } from "vue";
-import { useCardStore } from "@/Cards/cardState";
+import CardItem from '@/Cards/card-item/CardItem.vue'
+import { computed, onMounted } from 'vue'
+import { useCardStore } from '@/Cards/cardState'
 import { useRouter } from 'vue-router'
 import { useCartStore } from '@/Cart/cartState'
 
-
-
-
 //add to Cart
-const storeCart = useCartStore();
+const storeCart = useCartStore()
 const cart = computed(() => {
   return storeCart.cart
-});
+})
 const addToCart = (cart: any) => {
   console.log(cart)
 }
-const storeCards = useCardStore();
+const storeCards = useCardStore()
 const cards = computed(() => {
-  return storeCards.cards;
-});
+  return storeCards.cards
+})
 
 // render cards list
 onMounted(() => {
-  storeCards.fetchCards();
-
-});
+  storeCards.fetchCards()
+})
 
 const router = useRouter()
 
@@ -33,21 +29,27 @@ const router = useRouter()
 const itemClick = (name: string) => {
   router.push({
     name: 'card',
-    query: { 'card': name }
+    query: { card: name },
   })
 }
-
 </script>
 
 <template>
   <div>
-
     <div class="my-5">
-      <h2 class="flex justify-center text-4xl font-extrabold mb-8">Card List</h2>
+      <h2 class="flex justify-center text-4xl font-extrabold mb-8">
+        Card List
+      </h2>
     </div>
     <div class="flex flex-row justify-center static flex-wrap mb-8">
-      <CardItem class="my-3 mx-7 hover:shadow-2xl" v-for="card in cards" :key="card.id" :card__data="card"
-        @itemClick="itemClick" @addToCart="addToCart">List Rendering
+      <CardItem
+        class="my-3 mx-7 hover:shadow-2xl"
+        v-for="card in cards"
+        :key="card.id"
+        :card__data="card"
+        @itemClick="itemClick"
+        @addToCart="addToCart"
+        >List Rendering
       </CardItem>
     </div>
   </div>
