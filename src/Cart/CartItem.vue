39ffe8c08a72604c7ebdff61b34fdd79e1632670
Roman-wdesign <klauss.elephant@gmail.com>
2023-03-06 14:46:45 +0700

    cart typing

--- src/Cart/CartItem.vue
+++ src/Cart/CartItem.vue
@@ -1,34 +1,33 @@
 <script setup lang="ts">
-
-export interface cartItemData {
-    name: string
-    image: string
-    id: number
-}
-
+import type { cartItemData } from '@/shared/models/CartModel'
 
 export interface Props {
-    cart__item__data: cartItemData
+  cart__item__data: cartItemData<string, number>
 }
 const props = defineProps<Props>()
 </script>
 
-
 <template>
-    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <img class="rounded-lg" :src="props.cart__item__data.image" alt="" />
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {{ props.cart__item__data.name }}
-        </h5>
-        <div class="quantity-block flex justify-evenly">
-            <ButtonMain
-                class="text-white bg-blue-600 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mt-5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                -
-            </ButtonMain>
-            <ButtonMain
-                class="text-white bg-blue-600 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mt-5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                +
-            </ButtonMain>
-        </div>
+  <div
+    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+  >
+    <img class="rounded-lg" :src="props.cart__item__data.image" alt="" />
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      {{ props.cart__item__data.name }}
+    </h5>
+    <div class="quantity-block flex justify-evenly">
+      <ButtonMain
+        class="text-white bg-blue-600 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mt-5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        -
+      </ButtonMain>
+      <ButtonMain
+        class="text-white bg-blue-600 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mt-5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        +
+      </ButtonMain>
     </div>
+  </div>
 </template>
---
+++ src/shared/models/CartModel.ts
@@ -0,0 +1,7 @@
+interface cartItemData<T, Y> {
+    name: T
+    image: T
+    id: Y
+}
+
+export type { cartItemData }
